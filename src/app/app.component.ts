a5464a19774e8a5f72fa265401bb9086d9ce5e9c
Prince <psychoaimerhere@gmail.com>
2024-11-06 14:18:25 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgFor } from '@angular/common'
+import { ChildComponent } from "./child/child.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgFor, ChildComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ToDo';
+  title = 'Task List';
+  list:any[] = [];
+  data:number = 10;
+  parentChild:string = 'Data transfer from Parent to Child component'
+
+  addtask(task:string){
+    this.list.push({id:this.list.length,name:task})
+    console.warn(task,"added")
+  }
+
+  removeTask(id:number){
+    this.list = this.list.filter(task=>task.id !== id)
+  }
+
+  updateChild(){
+    this.data = Math.floor(Math.random() * 10)
+    console.warn(this.data,"generated")
+  }
 }
---
+++ src/app/child/child.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-child',
+  standalone: true,
+  imports: [],
+  templateUrl: './child.component.html',
+  styleUrl: './child.component.css'
+})
+export class ChildComponent {
+  @Input() item:number = 0;
+}
